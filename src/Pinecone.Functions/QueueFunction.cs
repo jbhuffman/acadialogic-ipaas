13e094adb960bf47ab82b97fbd8b375cbfbb560c
Jarad Huffman <jbhuffman@gmail.com>
2025-12-16 11:13:21 -0500

    building my queue functions for processing.  This is just a trigger test.

---
+++ src/Pinecone.Functions/QueueFunction.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Pinecone.Functions;
+
+public class QueueFunction
+{
+    private readonly ILogger<QueueFunction> _logger;
+
+    public QueueFunction(ILogger<QueueFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    // Triggers on messages in the "event-work" Storage Queue
+    [Function("QueueTriggerDemo")]
+    public void Run(
+        [QueueTrigger("event-work", Connection = "AzureWebStorage")]
+        string message)
+    {
+        _logger.LogInformation("Queue message processed: {Message}", message);
+    }
+}
